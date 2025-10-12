e6277008458770f66bdab91da6d2662030584201
ElVettorato <gabricarle2008@gmail.com>
2025-10-12 19:40:01 +0200

    try to adapt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
 # Reserve space for GAPPS
--include vendor/pixelos/config/BoardConfigReservedSize.mk
+-include vendor/custom/config/BoardConfigReservedSize.mk
 
 # Platform
 BOARD_HAS_MTK_HARDWARE := true
