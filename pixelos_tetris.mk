88cf04f9712ed32a3ef4e960bdc015ad7ac17f68
ElVettorato <gabricarle2008@gmail.com>
2025-10-12 19:40:30 +0200

    try to adapt

--- pixelos_tetris.mk
+++ pixelos_tetris.mk
@@ -10,12 +10,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common infinity stuff.
-$(call inherit-product, vendor/pixelos/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Inherit from tetris device
 $(call inherit-product, device/nothing/tetris/device.mk)
 
-PRODUCT_NAME := pixelos_tetris
+PRODUCT_NAME := custom_tetris
 PRODUCT_DEVICE := tetris
 PRODUCT_BRAND := Nothing
 PRODUCT_MANUFACTURER := Nothing
