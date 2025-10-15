0d7ae581dc37b20e5cecb24a638a7d4d7afea7bc
ElVettorato <gabricarle2008@gmail.com>
2025-10-15 20:16:19 +0200

    adapt to LMODroid

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
 # Reserve space for GAPPS
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/lmodroid/config/BoardConfigReservedSize.mk
 
 # Platform
 BOARD_HAS_MTK_HARDWARE := true
