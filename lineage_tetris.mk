b29af5e467aca219172b0bff80b0d9549b21345b
ElVettorato <gabricarle2008@gmail.com>
2025-10-15 20:15:36 +0200

    adapt to LMODroid

--- lineage_tetris.mk
+++ lineage_tetris.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common infinity stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 # Inherit from tetris device
 $(call inherit-product, device/nothing/tetris/device.mk)
